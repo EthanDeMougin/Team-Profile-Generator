38f0e45a14ac5baf89f2df8cf02b221d44f24106
Ethan DeMougin <ethandemougin@gmail.com>
2022-10-08 22:42:46 -0400

    finished some progress on index for engineer and manager roles as well as fixed some text for intern

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
 const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
 
 teamArray = [];
 
@@ -53,11 +54,11 @@ function runApp () {
             },
             {
                 type: "input",
-                name: "internOfficeNumber",
-                message: "What is the Intern's office number?"
+                name: "internSchool",
+                message: "What is the Intern's school?"
             }
         ]).then(answers => {
-            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internOfficeNumber);
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
             teamArray.push(intern);
             createTeam(); 
         });
@@ -67,19 +68,67 @@ function runApp () {
         inquirer.prompt ([
 
             {
-
+                type: "input",
+                name: "managerName",
+                message: "What is the Manager's Name?"
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the Manager's ID?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the Manager's email?"
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is the Manager's office number?"
             }
-        ])
-
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teamArray.push(manager);
+            createTeam(); 
+        });
     }
 
     function addEngineer() {
-        inquirer ([
+        inquirer.prompt ([
 
             {
-                
+                type: "input",
+                name: "engineerName",
+                message: "What is the Engineer's Name?"
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is the Engineer's ID?"
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is the Engineer's email?"
+            },
+            {
+                type: "input",
+                name: "engineerGitHub",
+                message: "What is the Engineer's GitHub?"
             }
-        ])
-
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGitHub);
+            teamArray.push(engineer);
+            createTeam(); 
+        });    
     }
+
+    function htmlBuilder () {
+        console.log("Team successfully created!");
+    }
+
+    create ();
 }
+
+runApp();
